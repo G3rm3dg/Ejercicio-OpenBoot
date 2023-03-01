0b814c12299bcd093579483f77770785617be2b2
GerleyMG <svilupp.g3rm3dg@gmail.com>
2023-02-28 21:39:20 -0500

    OpenBoot Ejm1

---
+++ open.java
@@ -0,0 +1,15 @@
+public class open {
+
+    public static int suma(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        int resultado = 0;
+        int a = 4;
+        int b = 6;
+        resultado = suma(a, b);
+        System.out.println(resultado);
+    }
+
+}
